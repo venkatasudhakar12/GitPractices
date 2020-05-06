bd3248ebbfd389830e42dd6aa90bcfae96eb6a56
Sudhakar <sudhakariosdeveloper@gmail.com>
2020-05-06 12:57:03 +0530

    Initial Commit

---
+++ GitPractices/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitPractices
+//
+//  Created by Sudhakar on 06/05/20.
+//  Copyright © 2020 Sudhakar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
