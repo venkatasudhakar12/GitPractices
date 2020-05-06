6ab50dcaef96c3f9f022cb76dc31f41840e993e8
Sudhakar <sudhakariosdeveloper@gmail.com>
2020-05-06 13:19:37 +0530

    Map view controller

---
+++ GitPractices/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  GitPractices
+//
+//  Created by Sudhakar on 06/05/20.
+//  Copyright © 2020 Sudhakar. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
